79042b95fcaada7e243106aec198a9c726ee5bf1
Aleksandra <abaibik@gmail.com>
2023-09-21 15:45:19 +0200

    implement radix card on home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import GlobalStyle from "../styles";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <Theme accentColor="blue" grayColor="olive" scaling="105%">
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </Theme>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
-import Link from "next/link";
+import {
+  Box,
+  Link,
+  Card,
+  Container,
+  Flex,
+  Heading,
+  Text,
+} from "@radix-ui/themes";
 
 export default function HomePage() {
   return (
     <>
-      <h1>This is a Cities App.</h1>
-      <Link href="/cities">To the cities ➡</Link>
+      <Card my={"7"} variant="classic" size={"4"} style={{ borderRadius: "0" }}>
+        <Container py={"6"}>
+          <Flex direction={"column"}>
+            <Heading as="h1" mb={"4"} size={"7"}>
+              This is a Cities App
+            </Heading>
+            <Text as="p" size={"4"}>
+              Hier you can find some information about cities
+            </Text>
+            <Box mt={"6"}>
+              <Link size="5" href="/cities">
+                To the cities ➡
+              </Link>
+            </Box>
+          </Flex>
+        </Container>
+      </Card>
     </>
   );
 }
